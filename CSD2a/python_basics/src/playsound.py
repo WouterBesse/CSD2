9c831e92b79ef079082de0928a0ac69a266de752
Wouter Besse <wouterbesse@gmail.com>
2020-09-07 22:23:18 +0200

    Fixed playsound.py: Optimized the script and fixed it so it works both on windows and mac

--- CSD2a/python_basics/src/playsound.py
+++ CSD2a/python_basics/src/playsound.py
@@ -1,34 +1,21 @@
-#This script should work, but I haven't gotten simpleaudio to work.
-
 import simpleaudio as sa
+import pathlib
+import os
 
 def samplespeler(hoeveelheid, sample):
     if hoeveelheid > 0:
-        if sample == 'WAP':
-            wave_obj = sa.WaveObject.from_wave_file('../media/WAP.wav')
+            proj_folder = pathlib.Path(f'{os.getcwd()}\{__file__}').parent.parent
+            filetype = ".wav"
+            samplefile = f'{sample}{filetype}'
+            file_to_open = str(proj_folder / 'media' / samplefile)
+            wave_obj = sa.WaveObject.from_wave_file(file_to_open)
             play_obj = wave_obj.play()
             play_obj.wait_done()
             hoeveelheid -= 1
             samplespeler(hoeveelheid, sample)
-        elif sample == 'RAP':
-            wave_obj = sa.WaveObject.from_wave_file('./media/RAP.wav')
-            play_obj = wave_obj.play()
-            play_obj.wait_done()
-            hoeveelheid -= 1
-            samplespeler(hoeveelheid, sample)
-        elif sample == 'FAP':
-            wave_obj = sa.WaveObject.from_wave_file('../media/FAP.wav')
-            play_obj = wave_obj.play()
-            play_obj.wait_done()
-            hoeveelheid -= 1
-            samplespeler(hoeveelheid, sample)
-        else:
-            print('Uw gekozen sample bestaat hier niet')
     else:
         print('Hopelijk heeft U genoten')
 
-print ('Welke sample wilt u spelen? (Kies uit WAP, RAP en FAP)')
-sample = input()
-print ('Hoe vaak wilt u deze sample afspelen?')
-hoeveelheid = int(input())
+sample = input('Welke sample wilt u spelen? (Kies uit WAP, RAP en FAP)')
+hoeveelheid = int(input('Hoe vaak wilt u deze sample afspelen?'))
 samplespeler(hoeveelheid, sample)
