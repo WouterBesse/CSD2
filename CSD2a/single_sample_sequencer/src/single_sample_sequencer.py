cc7fdd3f9d0035843a3dde3da77422278718b6c6
Wouter Besse <wouterbesse@gmail.com>
2020-09-08 16:30:15 +0200

    Updated single_sample_sequencer.py: The sequencer now works for drum samples and plays these well

--- CSD2a/single_sample_sequencer/src/single_sample_sequencer.py
+++ CSD2a/single_sample_sequencer/src/single_sample_sequencer.py
@@ -12,6 +12,19 @@ json_file = open(json_loc, 'r')
 json_pure = str(json_file.read(500))
 seq_data = json.loads(json_pure)
 
+#Load in samples and add these to a simple to acces dictionary
+snare = str(f'{proj_folder}\media\s.wav')
+kick = str(f'{proj_folder}\media\k.wav')
+hat = str(f'{proj_folder}\media\h.wav')
+empt = str(f'{proj_folder}\media\l.wav')
+sampledict = {
+    's': sa.WaveObject.from_wave_file(snare),
+    'k': sa.WaveObject.from_wave_file(kick),
+    'h': sa.WaveObject.from_wave_file(hat),
+    'x': sa.WaveObject.from_wave_file(empt)
+}
+
+
 #Get all information from the user
 def beatinput():
     beats = int(input('How many beats are there in a measure? (choose from 3, 4 or 5) '))
@@ -28,36 +41,38 @@ def beatinput():
 
 #Builds a sequencer location array, convert bpm to milliseconds
 def beatconverter(beats, measures, bpm):
-    waittime = (60000 / bpm / 16) * 0.001
+    waittime = (60000 / bpm / 8) * 0.001
     seqloc = [1, 1, 1]
-    sequencer(seqloc, waittime, beats, measures)
+    counter = 0
+    sequencer(seqloc, waittime, beats, measures, counter)
 
-def sequencer(seqloc, waittime, beats, measures):
-    print(seqloc)
-    if seqloc[0] < measures:
-        print (seqloc)
-        note = seq_data[str(beats)][seqloc[2] - 1]
-        print(note)
+#Plays samples according to given input
+def sequencer(seqloc, waittime, beats, measures, counter):
+    if seqloc[0] <= measures:
+        print(seqloc)
+        note = seq_data[str(beats)][counter]
         if note == 'k' or note == 's' or note == 'h' or note == 'x':
             samplefile = f'{note}.wav'
-            file_to_open = str(f'{proj_folder}\media\samplefile')
-            #wave_obj = sa.WaveObject.from_wave_file(file_to_open)
+            play_obj = sampledict[note].play()
             time.sleep(waittime)
-            if seqloc[2] == 16:
+            if seqloc[2] == 4:
                 if seqloc[1] == beats:
                     seqloc[0] += 1
                     seqloc[1] = 1
                     seqloc[2] = 1
-                    sequencer(seqloc, waittime, beats, measures)
+                    counter = 0
+                    sequencer(seqloc, waittime, beats, measures, counter)
                 else:
                     seqloc[1] += 1
                     seqloc[2] = 1
-                    sequencer(seqloc, waittime, beats, measures)
+                    counter += 1
+                    sequencer(seqloc, waittime, beats, measures, counter)
             else:
                 seqloc[2] += 1
-                sequencer(seqloc, waittime, beats, measures)
+                counter += 1
+                sequencer(seqloc, waittime, beats, measures, counter)
         else:
             print('Have a nice day :)')
 
-print(seq_data['3'][2])
+#Initiate functions
 beatinput()
