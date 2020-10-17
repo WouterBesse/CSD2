4768ae2f09020eaaacbb62913e9e9caa46d3a89d
Wouter M. Besse <wouterbesse@gmail.com>
2020-10-17 14:29:46 +0200

    Updated sample_sequencer_v20: Made the sequencer work on one thread

--- CSD2a/eindopdracht/src/sample_sequencer_v20.py
+++ CSD2a/eindopdracht/src/sample_sequencer_v20.py
@@ -4,7 +4,7 @@ import os
 import pathlib
 import sys
 import threading
-import time as tijd
+import time
 
 import simpleaudio as sa
 from midiutil import MIDIFile
@@ -12,18 +12,19 @@ from synthesizer import Player, Synthesizer, Waveform
 
 proj_folder = pathlib.Path(__file__).parent.parent
 
-# Define variable
+# Define script variables
 wait_time = 0
 measures = 0
 beats = 4
-drumlist = []
-notelist = []
+drum_list = []
+synth_list = []
 counter = 0
 seqloc = [1, 1, 1]
 drum_ready = 0
 synth_ready = 0
 notes = ['a', 'ais', 'b', 'c', 'cis', 'd', 'dis', 'e', 'f', 'fis', 'g', 'gis']
 hertz = 0
+base_time = 0
 
 
 # Initialise synth
@@ -33,12 +34,9 @@ synth = Synthesizer(osc1_waveform=Waveform.triangle, osc1_volume=0.2, osc2_freq_
 
 # Define midi variables
 degrees = []  # MIDI note number for drums
-degreesn = [] #MIDI note number for notes
 track = 0
-trackn = 1
 channel = 0
-time = [0]  # In beats
-timen = [0]
+midi_time = [0]  # In beats
 duration = 1  # In beats
 tempo = 60  # In BPM
 volume = 100  # 0-127, as per the MIDI standard
@@ -94,17 +92,13 @@ midi_conv = {
 # Add class for threading
 exitFlag = 0
 
-class Drumthread(threading.Thread):
+class seq_thread(threading.Thread):
     def __init__(self, thread_id, name):
         threading.Thread.__init__(self)
-        global seqloc, counter
         self.threadID = thread_id
         self.name = name
-        self.counter = counter
-        self.seqloc = seqloc
-
     def run(self):
-        dsequencer(self.name)
+        play_sound(self.name)
 
 class synThread(threading.Thread):
     def __init__(self, threadID, name):
@@ -118,15 +112,51 @@ class synThread(threading.Thread):
     def run(self):
         nsequencer(self.name)
 
-# Function to convert midi to hz
-def mtoh(x):
-    global hertz
-    a = 440
-    #hertz = (a / 32) * (2 ^ ((x - 9) / 12))
-    hertz = (a / 32) * pow(2, ((x - 9) / 12))
+# Function to update seqloc counter
+def update_seqloc():
+    global seqloc, beats
+    if seqloc[2] == 4:
+        if seqloc[1] == beats:
+            seqloc[0] += 1
+            seqloc[1] = 1
+            seqloc[2] = 1
+        else:
+            seqloc[1] += 1
+            seqloc[2] = 1
+    else:
+        seqloc[2] += 1
+
+# Funtion to play the drum sound according to beat number, checks if it's a legal drum note
+def play_drum(drum_note):
+    global drum_list
+    if drum_note.lower() == 'k' or drum_note.lower() == 's' or drum_note.lower() == 'h' or drum_note.lower() == 'x':
+        # Adds to the drum list for later midi export
+        drum_list.append(drum_note.lower())
+        # Plays the given note from a dictionary
+        play_obj = sampledict[drum_note.lower()].play()
+    else:
+        print('PANIEK!')
+
+# Function to play the synth sound according to note name
+def play_synth(synth_note):
+    global synth_list, wait_time
+    # Checks if the note is not a rest (x)
+    if synth_note != 'x':
+        # Plays the note, no need for conversions to hertz or midi because the synth library already does that
+        player.play_wave(synth.generate_constant_wave(str(synth_note.capitalize()), wait_time))
+        # Convert the note to midi
+        midi_note_raw = midi_conv.get(synth_note[:-1].lower())
+        if synth_note[:-1].isdigit():
+            midi_note = int(midi_note_raw) + (synth_note[-1] * 12)
+            synth_list.append(midi_note)
+        else:
+            synth_list.append(int(midi_note_raw))
+    else:
+        play_obj = sampledict[synth_note.lower()].play()
+        synth_list.append(synth_note.lower())
 
 # Get all information from the user
-def beatinput():
+def beat_input():
     global measures, beats, wait_time, tempo
     beats = int(input('How many beats are there in a measure? (choose from 3, 4 or 5) \n'))
     if beats == 3 or beats == 4 or beats == 5:
@@ -139,159 +169,58 @@ def beatinput():
             beat_converter(bpm)
         else:
             print('Wrong BPM amount')
-            beatinput()
+            beat_input()
     else:
         print('Wrong beat amount')
-        beatinput()
+        beat_input()
 
 
 # Builds a sequencer location array, convert bpm to milliseconds, also starts the sequencer and threading
 def beat_converter(bpm):
-    global wait_time, beats, measures
+    global wait_time, beats, measures, base_time
     wait_time = (60000 / bpm / 8) * 0.001
+    base_time = int(round(time.time() * 1000))
     screen_clear()
     # Create new threads
-    thread1 = Drumthread(1, 'drumthread')
-    thread2 = synThread(2, 'synthread')
+    thread1 = seq_thread(1, 'seq_thread')
+    #thread2 = synThread(2, 'synthread')
     thread1.start()
-    thread2.start()
+    #thread2.start()
     thread1.join()
-    thread2.join()
-    save_to_midi()
+    #thread2.join()
+    #save_to_midi()
 
-
-# Plays drum samples according to given input
-def dsequencer(threadName):
-    global measures, beats, wait_time, drumlist, drum_ready, synth_ready, counter, seqloc
-    # Checks if the given amount of measures has been played already, if not then executes the script
+def play_sound(threadName):
+    global beats, counter, seqloc, wait_time, base_time, measures
+    # Makes sure it stops when all measures are played
     while seqloc[0] <= measures:
-        # Necessary for multi threading, so it can switch to the other thread
+        # Exit flag for the threading to work
         if exitFlag:
             threadName.exit()
-        # Writes out the current transport location without adding an extra line
-        sys.stdout.write("\r " + str(seqloc))
-        sys.stdout.flush()
-        # Gets the note that needs to be played
-        note = seq_data[str(beats)][counter]
-        # Checks if it's a legal note
-        if note.lower() == 'k' or note.lower() == 's' or note.lower() == 'h' or note.lower() == 'x':
-            # Adds to the drumlist for later midi export
-            drumlist.append(note.lower())
-            # Plays the given note from a dictionary
-            play_obj = sampledict[note.lower()].play()
-            # Waits the desired amount of time
-            tijd.sleep(wait_time)
-            drum_ready = 1
-            while drum_ready == 0 or synth_ready == 0:
-                tijd.sleep(0.001)
-            counter_update(seqloc)
-        else:
-            print('\n Error, invalid note')
-    drum_ready = 1
+        # Writes the time that has elapsed since starting the sequencer
+        elapsed_time = int(round(time.time() * 1000)) - base_time
+        # Keeps track of the count
+        counter = seqloc[0] * seqloc[1] * seqloc[2]
+        # Which note in the sequence to play
+        note_to_play = seqloc[1] * seqloc[2] - 1
+        # Check if a beat needs to be played, multiplies the wait_time by 1000 to make it whole milliseconds
+        if elapsed_time / (int(wait_time * 1000)*2) >= counter:
+            # Write out beat number
+            sys.stdout.write("\r " + str(seqloc))
+            sys.stdout.flush()
+            # Gets the notes to be played
+            drum_note = seq_data[str(beats)][note_to_play]
+            synth_note = seq_data[str(f'{beats}s')][note_to_play]
+            # Plays the sounds
+            play_drum(drum_note)
+            play_synth(synth_note)
+            # Update the sequence counter
+            update_seqloc()
+        time.sleep(0.001)
 
-
-# Plays sequencer according to given input
-def nsequencer(threadName):
-    global measures, beats, wait_time, drumlist, drum_ready, synth_ready, hertz, synth_ready, counter, seqloc
-    # Checks if the given amount of measures has been played already, if not then executes the script
-    while seqloc[0] <= measures:
-        # Necessary for multi threading, so it can switch to the other thread
-        if exitFlag:
-            threadName.exit()
-        # Gets the note that needs to be played
-        note = seq_data[str(f'{beats}s')][counter]
-        if note != 'x':
-            player.play_wave(synth.generate_constant_wave(str(note.capitalize()), wait_time))
-            midinoteraw = midi_conv.get(note[:-1].lower())
-            # print(midinoteraw)
-            tijd.sleep(wait_time)
-            if note[:-1].isdigit():
-                midinote = int(midinoteraw) + (note[-1] * 12)
-                notelist.append(midinote)
-            else:
-                notelist.append(int(midinoteraw))
-        else:
-            play_obj = sampledict[note.lower()].play()
-            notelist.append(note.lower())
-            tijd.sleep(wait_time)
-        synth_ready = 1
-        # print(drum_ready)
-        while drum_ready == 0 or synth_ready == 0:
-            tijd.sleep(0.001)
-    synth_ready = 1
-    print(notelist)
-    if drum_ready == 1 and synth_ready == 1:
-        return()
-
-
-
-# Updates the counter
-def counter_update(seqloc):
-    global drum_ready, synth_ready, beats, counter
-    if seqloc[2] == 4:
-        if seqloc[1] == beats:
-            seqloc[0] += 1
-            seqloc[1] = 1
-            seqloc[2] = 1
-            counter = 0
-            drum_ready = 0
-            synth_ready = 0
-        else:
-            seqloc[1] += 1
-            seqloc[2] = 1
-            counter += 1
-    else:
-        seqloc[2] += 1
-        counter += 1
-
-# Script which converts all the played notes to midi data and saves this as a .mid file
-def save_to_midi():
-    global degrees, track, channel, time, duration, tempo, volume, degreesn, timen, trackn
-    for i in range(len(drumlist)):
-        # Adds an extra duration to the end of the previous note to make a rest
-        if drumlist[i] == 'x':
-            time[-1] += 1
-        else:
-            midinum = midi_conv.get(drumlist[i])
-            degrees.append(midinum)
-            time.append(0)
-
-    for i in range(len(notelist)):
-        # Adds an extra duration to the end of the previous note to make a rest
-        if notelist[i] == 'x':
-            timen[-1] += 1
-        else:
-            degreesn.append(notelist[i])
-            timen.append(0)
-
-    print(degrees)
-    # Midi export starts here
-    MyMIDI = MIDIFile(2)
-    MyMIDI.addTempo(1, 0, tempo)
-    tim = -1
-    timn = -1
-    for i, pitch in enumerate(degrees):
-        tim += time[i] + 1
-        MyMIDI.addNote(0, channel, pitch, tim, duration, volume)
-
-    print(timen)
-    print(degreesn)
-    for i, pitchn in enumerate(degreesn):
-        timn += timen[i] + 1
-        print(pitchn)
-        print(i)
-        print(timn)
-        print(timen[i])
-        MyMIDI.addNote(1, channel, pitchn, timn, duration, volume)
-
-    with open("CSD2a/single_sample_sequencer/media/Sequence.mid", "wb") as output_file:
-        MyMIDI.writeFile(output_file)
-
-    print('\n Have a nice day, your midi file has been saved :)')
-
-
-
-# Initiate script
 screen_clear()
-beatinput()
-# python3 csd2a/single_sample_sequencer/src/single_sample_sequencer.py
+beat_input()
+
+
+
+# python3 csd2a/eindopdracht/src/sample_sequencer_v20.py
